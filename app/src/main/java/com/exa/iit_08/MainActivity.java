f764dc136f3a16e46f004e67b4c629f4569e06b9
Md. Shahin Bashar <46506236+sbiitju@users.noreply.github.com>
2022-01-17 15:32:03 +0600

    shahin

--- app/src/main/java/com/exa/iit_08/MainActivity.java
+++ app/src/main/java/com/exa/iit_08/MainActivity.java
@@ -3,12 +3,35 @@ package com.exa.iit_08;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    EditText editText;
+    Button button;
+    String message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.firstTV);
+        editText=findViewById(R.id.firstET);
+        button=findViewById(R.id.firstBtn);
+        message=editText.getText().toString();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText().toString());
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                editText.setText("");
+            }
+        });
+
     }
 }
